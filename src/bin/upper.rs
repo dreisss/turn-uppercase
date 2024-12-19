176a67ed98b0a36adb3fdb22e438db9785c859ae
Davi Reis <dreisss.dev@gmail.com>
2024-12-19 10:42:09 -0300

    feat: update to use clipboard

--- src/bin/upper.rs
+++ src/bin/upper.rs
@@ -1,8 +1,14 @@
+extern crate clipboard;
+use clipboard::{ClipboardContext, ClipboardProvider};
 use std::env::args;
 
 fn main() {
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+
     let mut input: Vec<String> = args().collect();
     input.remove(0);
 
-    println!("{}", input.join(" ").to_uppercase());
+    let output = input.join(" ").to_uppercase();
+    println!("{}", output);
+    ctx.set_contents(output).unwrap();
 }
